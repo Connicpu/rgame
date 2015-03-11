16428e82cf8005ca61da7ecd73deea74c2e79bba
Connor Hilarides <connorcpu@live.com>
2015-03-10 19:49:23 -0700

    grr

--- src/systems/graphics/sprite_fragment.glsl
+++ src/systems/graphics/sprite_fragment.glsl
@@ -2,8 +2,8 @@
 
 uniform sampler2D texture;
 
-varying vec4 v_color;
-varying vec2 v_tex_coord;
+in vec4 v_color;
+in vec2 v_tex_coord;
 
 void main() {
     gl_FragColor = v_color * texture2D(texture, v_tex_coord);
--- src/systems/graphics/sprite_vertex.glsl
+++ src/systems/graphics/sprite_vertex.glsl
@@ -6,8 +6,8 @@ attribute vec2 position;
 attribute vec2 tex_coord;
 attribute vec4 color;
 
-varying vec4 v_color;
-varying vec2 v_tex_coord;
+out vec4 v_color;
+out vec2 v_tex_coord;
 
 void main() {
     gl_Position = vec4(position, 0.0, 1.0) * camera;
